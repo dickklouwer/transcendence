8bce0c0757ea23712d0b08b6cb7fb459277936d6
Theodorus johannes nicolaas Klouwer <tklouwer@f1r6s13.codam.nl>
2024-04-25 17:18:16 +0200

    NestJS with Docker and PostgreSQL up and running.

---
+++ containers/svelte/rollup.config.js
@@ -0,0 +1,10 @@
+import resolve from '@rollup/plugin-node-resolve';
+
+export default {
+  input: 'src/main.js',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'cjs'
+  },
+  plugins: [resolve()]
+};
