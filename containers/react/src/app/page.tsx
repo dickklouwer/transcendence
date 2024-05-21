0e09c6a4f0586a75d60b7821051326d079ec8687
Dick Klouwer <62849082+dickklouwer@users.noreply.github.com>
2024-05-21 16:30:14 +0200

    Add files via upload

--- containers/react/src/app/page.tsx
+++ containers/react/src/app/page.tsx
@@ -59,7 +59,7 @@ function Menu({ navigateToHome }) {
   <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded" onClick={() => alert('Multiplayer')}>
     Multiplayer
   </button>
-  <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded" onClick={navigateToLogin}>
+  <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded" onClick={requestIdleCallback='http://localhost:2424/api/auth/signin'}>
     Login
   </button>
   <button className="text-blue-500 mt-4" onClick={navigateToHome}>
