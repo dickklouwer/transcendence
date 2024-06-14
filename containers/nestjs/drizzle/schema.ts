00e5245e9376adee74c2cf589e8f7575a68a495c
Bram Provoost <bprovoos@student.codam.nl>
2024-06-14 16:36:56 +0200

    database tables init

--- containers/nestjs/drizzle/schema.ts
+++ containers/nestjs/drizzle/schema.ts
@@ -4,28 +4,22 @@ import {
   text,
   integer,
   pgSchema,
-  boolean,
-  pgEnum,
+  boolean
 } from 'drizzle-orm/pg-core';
 
 export const mySchema = pgSchema('pong');
 
-export const user_state = pgEnum('user_state', [
+export const user_state = mySchema.enum('user_state', [
   'Online',
   'Offline',
   'In-Game',
   'Idle',
 ]);
 
-export const message_state = pgEnum('message_state', [
-  'sent',
-  'delivered',
-  'read',
-]);
-
-export const users = mySchema.table('user', {
+export const users = mySchema.table('users', {
   intra_user_id: integer('intra_user_id').primaryKey(),
-  name: text('name').notNull().unique(),
+  user_name: text('user_name').notNull().unique(),
+  nick_name: text('nick_name').default(null),
   token: text('token').notNull().unique(),
   email: text('email').notNull().unique(),
   state: user_state('state').notNull().default('Online'),
@@ -33,21 +27,23 @@ export const users = mySchema.table('user', {
 });
 
 export const friends = mySchema.table('friends', {
-  user_id_one: integer('user_id_one').notNull(),
-  user_id_two: integer('user_id_two').notNull(),
+  friend_id: serial('friend_id').primaryKey(),
+  user_id_send: integer('user_id_send').notNull(),
+  user_id_receive: integer('user_id_receive').notNull(),
   is_approved: boolean('is_approved').notNull().default(false), 
 });
 
 export const groupChats = mySchema.table('group_chats', {
   group_chat_id: serial('group_chat_id').primaryKey(),
   group_name: text('group_name').notNull(),
-  group_password: text('group_password').notNull(),
+  group_password: text('group_password').default(null),
+  group_image: text('group_image').default(null),
   created_at: timestamp('created_at').defaultNow(),
 });
 
 export const groupChatsUsers = mySchema.table('group_chats_users', {
+  group_chat_user_id: serial('group_chat_user_id').primaryKey(),
   group_chat_id: integer('group_chat_id')
-    .primaryKey()
     .references(() => groupChats.group_chat_id),
   intra_user_id: integer('intra_user_id').references(() => users.intra_user_id),
   is_owner: boolean('is_owner').notNull().default(false),
@@ -59,7 +55,7 @@ export const groupChatsUsers = mySchema.table('group_chats_users', {
 
 export const messages = mySchema.table('messages', {
   message_id: serial('message_id').primaryKey(),
-  sender_id: integer('sender_id').references(() => users.intra_user_id),
+  sender_id: integer('sender_id').references(() => users.intra_user_id).notNull(),
   reciever_id: integer('reciever_id'),
   group_chat_id: integer('group_chat_id'),
   message: text('message').notNull(),
@@ -67,9 +63,9 @@ export const messages = mySchema.table('messages', {
 });
 
 export const messageStatus = mySchema.table('message_status', {
-  message_id: integer('message_id')
-    .primaryKey()
-    .references(() => messages.message_id),
-  user_id: integer('user_id').references(() => users.intra_user_id),
-  status: message_state('status').notNull().default('sent'),
+  message_status_id: serial('message_status_id').primaryKey(),
+  message_id: integer('message_id').references(() => messages.message_id).notNull(),
+  reciever_id: integer('reciever_id').references(() => users.intra_user_id).notNull(),
+  receivet_at: timestamp('receivet_at').default(null),
+  read_at: timestamp('read_at').default(null),
 });
