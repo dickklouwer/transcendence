c10eb59c71977464cba9bd159e9f940a3b228a01
Victor <victor.brouwer@hotmail.com>
2024-10-11 11:59:36 +0200

    simple version

--- apps/nestjs/src/pong/pong.module.ts
+++ apps/nestjs/src/pong/pong.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MultiplayerPongGateway } from './multiplayerPong.gateway';
-import { SingleplayerPongGateway } from './singleplayerPong.gateway';
 import { PowerUpPongGateway } from './powerUpPong.gateway';
 
 @Module({
-  providers: [SingleplayerPongGateway, MultiplayerPongGateway, PowerUpPongGateway],
+  providers: [MultiplayerPongGateway, PowerUpPongGateway],
 })
 export class PongModule {}
--- apps/react/src/app/pong_menu/page.tsx
+++ apps/react/src/app/pong_menu/page.tsx
@@ -1,56 +1,24 @@
 "use client";
 
-import { useState } from 'react';
 import Link from 'next/link';
-import { userSocket } from '../layout';
 
 export default function Menu() {
-  const [selectedMode, setSelectedMode] = useState<string | null>(null);
 
   function renderInitialOptions() {
     return (
       <>
         <Link
           className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
-          href="#"
-          onClick={() => setSelectedMode('singleplayer')}
+          href="/pong/singleplayer_PowerUp"
         >
           Single Player
         </Link>
         <Link
           className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
-          href="#"
-          onClick={() => setSelectedMode('multiplayer')}
+          href="/pong/multiplayer"
         >
           Multiplayer
         </Link>
-      </>
-    );
-  }
-
-  function renderPowerUpOptions() {
-    const baseHref = selectedMode === 'singleplayer' ? '/pong/singleplayer' : '/pong/multiplayer';
-
-    return (
-      <>
-        <Link
-          className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
-          href={`${baseHref}`}
-        >
-          Play without Power Ups
-        </Link>
-        <Link
-          className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
-          href={`${baseHref}_PowerUp`}
-        >
-          Play with Power Ups
-        </Link>
-        <button
-          className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
-          onClick={() => setSelectedMode(null)}
-        >
-          Back
-        </button>
       </>
     );
   }
@@ -59,9 +27,9 @@ export default function Menu() {
     <div className="static space-y-4 w-full">
       <div className="relative flex flex-col items-center justify-center flex-grow space-y-4 min-h-screen">
         <h2 className="text-2xl font-bold text-center mt-8">
-          {selectedMode ? `Choose Power-Up Option for ${selectedMode.charAt(0).toUpperCase() + selectedMode.slice(1)}` : 'Choose Your Game Mode'}
+          Choose Your Game Mode
         </h2>
-        {selectedMode ? renderPowerUpOptions() : renderInitialOptions()}
+        {renderInitialOptions()}
         <Link className="text-blue-500 mt-4" href={'/'}>
           Back to Home
         </Link>
