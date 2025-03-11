702827a6a9ae0422d821ede06399f3d7bb9ef672
Jisse Meruma <jissemeruma@gmail.com>
2025-03-11 19:19:47 +0100

    DB INSANE URL SHIZZLE

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,4 +1,9 @@
 import { defineConfig } from 'drizzle-kit';
+import * as dotenv from 'dotenv';
+
+dotenv.config({
+  path: '../../.env',
+});
 
 if (!process.env.DATABASE_URL_LOCAL) {
   throw new Error('DATABASE_URL_LOCAL not found in environment');
