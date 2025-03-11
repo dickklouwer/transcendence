9f71086883f6a26da0d4c092feebf0d6107706bd
Jisse Meruma <jissemeruma@gmail.com>
2025-03-11 18:46:41 +0100

    DB credentials changed to ENV

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'drizzle-kit';
 
+if (!process.env.DATABASE_URL_LOCAL) {
+  throw new Error('DATABASE_URL_LOCAL not found in environment');
+}
+
 export default defineConfig({
   schema: './src/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: "postgres://postgres:postgres@localhost:5432/postgres"
+    url: process.env.DATABASE_URL_LOCAL,
   },
   migrations: { schema: './drizzle' },
   verbose: true,
